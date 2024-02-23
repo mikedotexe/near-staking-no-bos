c6f0ff8fec10cea1b5884b34259b920241fac636
Mike Purvis <mikedotexe@gmail.com>
2024-02-23 11:43:26 -0800

    Revert "Revert "follow NextJS webpack config instructions""
    This reverts commit a98e17d1af181a817dfe1dfb467501dd5041a595.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     transpilePackages: ["@near-wallet-selector/wallet-connect"],
     experimental: {
         esmExternals: "loose"
+    },
+    webpack: config => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding')
+        return config
     }
 };
 
